620391d6e2e8c450b1db25580857bca2ef95ecc6
yeezick <ramerick5@gmail.com>
2021-08-20 15:55:42 -0400

    intiating dev branch

--- src/layout/header/Header.jsx
+++ src/layout/header/Header.jsx
@@ -1,9 +1,43 @@
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+import {FaBars} from "react-icons/fa"
 import "./header.css"
 
 const Header = () => {
+
+  const [showMenu, setShowMenu] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 425) {
+        setShowMenu(false);
+      }
+    }
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  },[])
+
   return (
-    <nav>
-      I am the navbar
+    <nav className="navbar">
+      <Link className="nav-left" to="/">
+        <img src="" alt="react"/>
+        <h1 className="nav-title">Erick Manrique</h1>
+      </Link>
+
+      <div className="nav-right">
+        <Link className="nav-link" to="/about">
+          About
+        </Link>
+        <Link className="nav-link" to="/portfolio">
+          Portfolio
+        </Link>
+        <Link className="nav-link" to="/about">
+          About
+        </Link>
+
+      </div>
     </nav>
   )
 }
