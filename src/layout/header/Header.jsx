b75264c105a1007b84675b03b404147f7d6fbfcc
yeezick <ramerick5@gmail.com>
2021-08-20 18:11:20 -0400

    accomplishing mobile nav bar menu

--- src/layout/header/Header.jsx
+++ src/layout/header/Header.jsx
@@ -1,45 +1,81 @@
-import { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
-import {FaBars} from "react-icons/fa"
-import "./header.css"
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "./header.css";
 
 const Header = () => {
-
   const [showMenu, setShowMenu] = useState(false);
+  const [mobileNav, setMobileNav] = useState(true);
 
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth > 425) {
         setShowMenu(false);
+        setMobileNav(false);
       }
-    }
-    window.addEventListener('resize', handleResize)
+    };
+    window.addEventListener("resize", handleResize);
     return () => {
-      window.removeEventListener('resize', handleResize)
-    }
-  },[])
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <nav className="navbar">
+      {showMenu ? (
+        <FaTimes
+          size={30}
+          className="nav-menu"
+          onClick={() => {
+            setShowMenu(!showMenu);
+          }}
+        />
+      ) : (
+        <FaBars
+          size={30}
+          className="nav-menu"
+          onClick={() => {
+            setShowMenu(!showMenu);
+          }}
+        />
+      )}
       <Link className="nav-left" to="/">
-        <img src="" alt="react"/>
+        <img src="" alt="react" />
         <h1 className="nav-title">Erick Manrique</h1>
       </Link>
 
-      <div className="nav-right">
-        <Link className="nav-link" to="/about">
-          About
-        </Link>
-        <Link className="nav-link" to="/portfolio">
-          Portfolio
-        </Link>
-        <Link className="nav-link" to="/about">
-          About
-        </Link>
-
-      </div>
+        {!mobileNav ? (
+          <div className="nav-right">
+            <Link className="nav-link" to="/about">
+              About
+            </Link>
+            <Link className="nav-link" to="/portfolio">
+              Portfolio
+            </Link>
+            <Link className="nav-link" to="/about">
+              About
+            </Link>
+          </div>
+        ) : (
+          <>
+            {showMenu ? (
+              <div className="mobile-nav-right">
+                <Link className="nav-link" to="/about">
+                  About
+                </Link>
+                <Link className="nav-link" to="/portfolio">
+                  Portfolio
+                </Link>
+                <Link className="nav-link" to="/about">
+                  About
+                </Link>
+              </div>
+            ) : null}
+          </>
+        )}
+   
     </nav>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
