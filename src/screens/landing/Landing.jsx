49428af453e65e0b7a7362c3cb57473ca0770766
yeezick <ramerick5@gmail.com>
2021-08-20 20:27:09 -0400

    adding atom animations -leaving off attempting to stabilize nucles -need to transfer work and about links to interior ring

--- src/screens/landing/Landing.jsx
+++ src/screens/landing/Landing.jsx
@@ -1,12 +1,46 @@
-// import Layout from "./../layout/layout"
-import "./landing.css"
+import "./landing.css";
 
 const Landing = () => {
   return (
     <div>
-      Landing screen
-    </div>
-  )
-}
+      <section className="intro app-section">
+        <p className="intro-hello">Hello</p>
+        <p className="intro-me">
+          I'm Erick, a software engineer. Welcome to my portfolio
+        </p>
+        <p className="intro-options">What would you like to see first?</p>
+      </section>
 
-export default Landing
+      <div className="atom">
+        <div className="exterior-ring">
+          
+          <div className="interior-ring">
+            <div className="interior-orbit">
+              <div className="interior-circle"></div>
+              <div className="interior-circle"></div>
+            </div>
+          </div>
+
+          <div className="exterior-circle"></div>
+          <div className="exterior-circle"></div>
+          <div className="exterior-circle"></div>
+          <div className="exterior-circle"></div>
+          <div className="exterior-circle">About</div>
+          <div className="exterior-circle"></div>
+          <div className="exterior-circle"></div>
+          <div className="exterior-circle">Work</div>
+          <div className="nucleus">MyPic</div>
+        </div>
+
+        {/* <div className="interior-ring">
+          <div className="interior-circle"></div>
+          <div className="interior-circle"></div>
+        </div> */}
+
+        {/* <div className="nucleus">MyPic</div> */}
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
