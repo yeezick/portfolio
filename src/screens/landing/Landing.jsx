a6f60ec392dfe3bc9b4d6d6934482ff00ea5be55
yeezick <ramerick5@gmail.com>
2021-08-23 16:06:41 -0400

    atom now not so messed up on esktop, adding a statehcange to atom on click

--- src/screens/landing/Landing.jsx
+++ src/screens/landing/Landing.jsx
@@ -1,11 +1,24 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-import  {BsFillPersonLinesFill} from "react-icons/bs"
-import {FaCode} from "react-icons/fa" //close contender FaLaptopCode
-import resPic from "../../images/resPic.jpeg"
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import { FaCode } from "react-icons/fa"; //close contender FaLaptopCode
+import resPic from "../../images/resPic.jpeg";
 import "./landing.css";
 
 const Landing = () => {
+  const [toggleDetail, setToggleDetail] = useState(0);
+
+  const handleAtomClick = () => {
+    console.log(toggleDetail);
+    if (toggleDetail === 2) {
+      setToggleDetail(0);
+    }
+    else {
+      setToggleDetail(toggleDetail + 1);
+    }
+  };
+
   return (
     <div>
       <section className="intro app-section">
@@ -16,15 +29,25 @@ const Landing = () => {
         <p className="intro-options">What would you like to see first?</p>
       </section>
 
-      <div className="atom">
+      <div
+        id="atom"
+        className={
+          toggleDetail === 0
+            ? "atom"
+            : toggleDetail === 1
+            ? "atom atom-active-about"
+            : "atom atom-active-portfolio"
+        }
+        onClick={handleAtomClick}
+      >
         <div className="exterior-ring">
           <div className="interior-ring">
             <div className="interior-orbit">
               <Link to="/portfolio" className="interior-circle">
-                <BsFillPersonLinesFill size={30}/>
+                <BsFillPersonLinesFill size={30} />
               </Link>
               <Link to="/about" className="interior-circle">
-               <FaCode size={40}/>
+                <FaCode size={40} />
               </Link>
             </div>
           </div>
@@ -37,7 +60,7 @@ const Landing = () => {
           <div className="exterior-circle"></div>
           <div className="exterior-circle"></div>
           <div className="exterior-circle"></div>
-          <img src={resPic} alt="myself" className="nucleus"/>
+          <img src={resPic} alt="myself" className="nucleus" />
         </div>
 
         {/* <div className="interior-ring">
