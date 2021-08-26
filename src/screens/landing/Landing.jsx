52bbd4b40ca05dae22ef5666c7a9db583edb6a5b
yeezick <ramerick5@gmail.com>
2021-08-26 01:51:53 -0400

    - adjusting desktop view for atom on landing - decreasing hover duration for technical icons

--- src/screens/landing/Landing.jsx
+++ src/screens/landing/Landing.jsx
@@ -35,9 +35,9 @@ const Landing = () => {
       <div className="atom-section">
         <h3 className="section-title">About Me</h3>
         <p className="section-text">
-          I'm a 22 year old developer, with a huge passion for sicence and
-          astronomy. It's apparent to me that being on the cutting edge of
-          technology is equivalent to contributing to humanity's evolution.
+          I'm a 22 year old developer, with a huge passion for science and
+          astronomy. My deep admiration for these make it apparent to me that being on the cutting edge of
+          technology is equivalent to contributing to humanity's evolution. That's exactly where I want to be.
         </p>
         <Link to="/about">
           <button className="project-button">
@@ -54,8 +54,8 @@ const Landing = () => {
         <h3 className="section-title">Portfolio</h3>
         <p className="section-text">
           While I specialize working with JavaScript and the MERN stack, my
-          exposure with Ruby on Rails has given me the insight on how the
-          concepts behind languages and frameworks trully transfer. It allows me
+          exposure with Ruby on Rails has given me necessary insight on how the
+          concepts behind languages and frameworks truly transfer; allowing me
           the confidence to immerge myself into uncomfortable territory with an
           everlasting passion to learn and excel.
         </p>
