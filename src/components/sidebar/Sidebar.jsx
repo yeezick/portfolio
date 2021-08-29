877a18ada026db088dd68fd5d7053671ebaf30ce
yeezick <ramerick5@gmail.com>
2021-08-29 16:25:52 -0400

    adding styling to sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,41 +1,41 @@
-import { useState } from "react"
-import {SiLinkedin} from "react-icons/si"
-import {RiCloseFill, RiGithubLine, RiTwitterLine, RiFileList3Line} from "react-icons/ri"
-import {HiOutlineAtSymbol} from "react-icons/hi"
+import { useState } from "react";
+import { SiLinkedin } from "react-icons/si";
+import {
+  RiCloseFill,
+  RiGithubLine,
+  RiTwitterLine,
+  RiFileList3Line,
+} from "react-icons/ri";
+import { HiOutlineAtSymbol } from "react-icons/hi";
 
-import "./sidebar.css"
+import "./sidebar.css";
 
 const Sidebar = () => {
-
   const [showSidebar, setShowSidebar] = useState(false);
 
   const handleSidebar = () => {
-    setShowSidebar(!showSidebar)
-  }
+    setShowSidebar(!showSidebar);
+  };
 
   return (
     <div className="sidebar-wrapper">
-    <div className="sidebar" onClick={handleSidebar}>
-    <p className="sidebar-label">CONTACT ME</p>  
-    </div>
-    
-    
+      <div className="sidebar" onClick={handleSidebar}>
+        <p className="sidebar-label">CONTACT</p>
+      </div>
       <div className={showSidebar ? "sidebar-menu active" : "sidebar-menu"}>
-      <div className="sidebar-content" onClick={handleSidebar}>
-        <div className="sidebar-toggle">
-        <RiCloseFill size={40}/>
+        <div className="sidebar-content" onClick={handleSidebar}>
+          <div className="sidebar-toggle">
+            <RiCloseFill size={40} />
+          </div>
+          <RiGithubLine size={40} />
+          <SiLinkedin size={40} />
+          <RiTwitterLine size={40} />
+          <HiOutlineAtSymbol size={40} />
+          <RiFileList3Line size={40} />
         </div>
-         <RiGithubLine size={40}/>
-        <SiLinkedin size={40}/>
-        <RiTwitterLine size={40}/>
-         <HiOutlineAtSymbol size={40}/>
-    <RiFileList3Line size={40}/>
       </div>
-      </div>
-      </div>
+    </div>
+  );
+};
 
-
-  )
-}
-
-export default Sidebar
+export default Sidebar;
