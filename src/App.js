bd8805c606a6218058ad5cc21a86417c36ae83be
yeezick <ramerick5@gmail.com>
2021-09-26 22:09:08 -0400

    particles -installing particles js (had to force a legacy-peer-deps command) - configuring particles json for space-like background

--- src/App.js
+++ src/App.js
@@ -5,14 +5,20 @@ import Landing from "./screens/landing/Landing"
 import About from "./screens/about/About"
 import Portfolio from "./screens/portfolio/Portfolio"
 import Sidebar from "./components/sidebar/Sidebar"
+import Particles from "react-particles-js"
+
+import particleData from "./particlesjs-config.json"
 import "./App.css";
+import { configure } from "@testing-library/dom";
 
 function App() {
 
 
   return (
+    <>
     <Layout className="App">
-      <Sidebar/>
+    <Particles  id="particles-js" params={particleData}/>
+      {/* <Sidebar/>
      <Route exact path="/">
        <Landing/>
      </Route>
@@ -21,8 +27,9 @@ function App() {
      </Route>
      <Route path="/portfolio">
        <Portfolio/>
-     </Route>
+     </Route> */}
     </Layout>
+    </>
   );
 }
 
