ef2a2bff48a92d83e60d7e6d1d6ccb47fb5f14ba
yeezick <ramerick5@gmail.com>
2021-11-17 18:57:45 -0500

    adding a favicon (me) and getting imports ready to replace atoms with tech icons

--- src/animations/atom/Atom.jsx
+++ src/animations/atom/Atom.jsx
@@ -1,6 +1,31 @@
-import resPic from "../../images/resPic.jpeg";
-
 import "./Atom.css"
+import resPic from "../../images/resPic.jpeg";
+import {
+  SiJavascript,
+  SiHtml5,
+  SiCss3,
+  SiRuby,
+  SiRails,
+  SiReact,
+  SiPostgresql,
+  SiBootstrap,
+  SiJquery,
+  SiMongodb,
+  SiNodeDotJs,
+} from "react-icons/si";
+
+{/* <SiJquery size={40} color="#78cff5" />
+<SiHtml5 size={40} color="#f06529" />
+<SiCss3 size={40} color="#2965f1" />
+<SiJavascript size={40} color="#F0DB4F" />
+<SiNodeDotJs size={40} color="#68A063" />
+<SiMongodb size={40} color="#3FA037" />
+<SiReact size={40} color="#61DBFB" />
+<SiRuby size={40} color="#cc0000" />
+<SiRails size={60} color="#cc0000" />
+<SiPostgresql size={40} color="#0064a5" />
+<SiBootstrap size={40} color="#563d7c" /> */}
+
 
 const Atom = ({toggleDetail, handleAtomClick}) => {
   return (
