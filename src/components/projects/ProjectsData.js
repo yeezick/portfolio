980764ec981176d056ef55ab267bee63b2410267
yeezick <ramerick5@gmail.com>
2022-01-04 20:56:23 -0500

    including not another todo app to project data

--- src/components/projects/ProjectsData.js
+++ src/components/projects/ProjectsData.js
@@ -1,31 +1,53 @@
 import iNetwork from "../../images/iNetwork.png";
 import homeio from "../../images/homeio.png";
 import dignifiedWiners from "../../images/dignified-winers.png";
+import notAnotherTodoApp from "../../images/notAnotherTodoApp.png";
 
 export const allProjects = [
   {
-    title: "iNetwork",
-    url: "https://inetwork.netlify.app/",
+    featured: true,
     github: "https://github.com/yeezick/iNetwork",
     img: iNetwork,
-    text: "A casual online alternative to the formal resume, users can submit    their information through a form to render an 'business card.' Full CRUD functionality implemented via AirTable.",
     techs: "React with JS, AOS, and Bootstrap.",
+    text: "A casual online alternative to the formal resume, users can submit    their information through a form to render an 'business card.' Full CRUD functionality implemented via AirTable.",
+    title: "iNetwork",
+    url: "https://inetwork.netlify.app/",
   },
   {
+    featured: true,
+    github: "https://github.com/yeezick/iNetwork",
+    // img: homeiov2,
+    techs: "React with JS, AOS, and Bootstrap.",
+    text: "A casual online alternative to the formal resume, users can submit    their information through a form to render an 'business card.' Full CRUD functionality implemented via AirTable.",
     title: "Home.io",
-    url: "https://homeio.netlify.app/",
+    url: "https://inetwork.netlify.app/",
+  },
+  {
+    featured: true,
     github: "https://github.com/yeezick/home.io-v2",
     img: homeio,
-    text: "Conceptually forked from the iPhone widget screen, home.io gives signed-up users access to a personalized home screen rendering data from up to 7 API’s.",
     techs: "Ruby on Rails and React.",
+    text: "Conceptually forked from the iPhone widget screen, home.io gives signed-up users access to a personalized home screen rendering data from up to 7 API’s.",
+    title: "Home.io 2.0.0",
+    url: "https://homeio.netlify.app/",
   },
   {
-    title: "Dignified Winers",
-    url: "https://dignified-winers.netlify.app/",
+    featured: true,
     github: "https://github.com/jessa-clark/dignified-winers",
     img: dignifiedWiners,
-    text: "Dignified Winers was collaboration between 4 talented developers and conceived as a request by a mock-client; a luxury magazine featuring ranked wines while allowing users to sign up and contribute.",
     techs: "the MERN Stack, aided by Insomnia, Heroku, and Netlify.",
+    text: "Dignified Winers was collaboration between 4 talented developers and conceived as a request by a mock-client; a luxury magazine featuring ranked wines while allowing users to sign up and contribute.",
+    title: "Dignified Winers",
+    url: "https://dignified-winers.netlify.app/",
+  },
+  {
+    featured: true,
+    github: "https://github.com/yeezick/react-todo",
+    img: notAnotherTodoApp,
+    techs: "React",
+    text: "I was challenged by a senior developer mentor to build a Todo app from scratch without tutorials, using only React to do so. This was the result!",
+    title: "Not Another Todo App",
+    url: "https://yeezick.github.io/react-todo/",
   },
 ];
 
