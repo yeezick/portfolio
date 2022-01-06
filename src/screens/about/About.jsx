6cbafd568daedf2f96496bbe2b8d28c3e975ae92
yeezick <ramerick5@gmail.com>
2022-01-06 09:47:27 -0500

    netlify build failing due to inconsistent case sensitivity on github'

--- src/screens/about/About.jsx
+++ src/screens/about/About.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { NasaPod } from "../../components/nasaPod/NasaPod";
+import { NasaPod } from "../../components/NasaPod/NasaPod.jsx";
 import "./about.css";
 
 const About = () => {
