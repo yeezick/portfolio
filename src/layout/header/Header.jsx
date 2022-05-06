3afe5bf69658932055908dd2166edf14f91c95e0
yeezick <ramerick5@gmail.com>
2022-05-06 18:59:01 -0400

    update resume with ATS friendly version

--- src/layout/header/Header.jsx
+++ src/layout/header/Header.jsx
@@ -59,7 +59,7 @@ const Header = () => {
           </Link>
           <a
             className="nav-link"
-            href="https://drive.google.com/file/d/1SssJSwm_AR1MCkRId8jzCILhBuifIcG4/view?usp=sharing"
+            href="https://docs.google.com/document/d/1g8NDyGHyGl2-6QS3X1DXBa2CCApWMQJnwUSBNb96tt8/edit?usp=sharing"
             target="_blank"
             rel="noreferrer"
           >
@@ -90,7 +90,7 @@ const Header = () => {
               </Link>
               <a
                 className="nav-link"
-                href="https://drive.google.com/file/d/1SssJSwm_AR1MCkRId8jzCILhBuifIcG4/view?usp=sharing"
+                href="https://docs.google.com/document/d/1g8NDyGHyGl2-6QS3X1DXBa2CCApWMQJnwUSBNb96tt8/edit?usp=sharing"
                 target="_blank"
                 rel="noreferrer"
               >
